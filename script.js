558db198a2cbf77f5273df2a418776fd451e732c
yuliia <pustovayajulia@gmail.com>
2023-05-09 14:53:34 +0300

    Redone for while

--- script.js
+++ script.js
@@ -13,91 +13,110 @@
 //   Знайти суму його парних дільників.
 //   Надрукувати повну таблицю множення від 1 до 10.
 
-let res = '';
-let i;
-for (i = 10; i < 21; i++) {
-  res = res + i + ',';
-}
 console.log('Вивести на сторінку в один рядок через кому числа від 10 до 20');
+let res = '';
+let inc = 10;
+while (inc <= 20) {
+  res = res + inc + ',';
+  inc++;
+}
 console.info(`i = ${res}`);
 
-let res2 = '';
-for (i = 10; i < 21; i++) {
-  res2 = res2 + Math.pow(i,
-    2) + ',';
-}
 console.log('Вивести квадрати чисел від 10 до 20');
-console.info(`i = ${res2}`);
-
-let res3 = '';
-for (i = 1; i < 10; i++) {
-  res3 = res3 + 7 * i + ' ';
+res = '';
+inc = 10;
+while (inc <= 20) {
+  res = res + Math.pow(inc,
+    2) + ',';
+  inc++;
 }
+console.info(`i = ${res}`);
+
 console.log('Вивести таблицю множення на 7');
-console.info(`${res3}`);
-
-let res4 = 0;
-for (i = 1; i < 16; i++) {
-  res4 = res4 + i;
+res = '';
+inc = 1;
+while (inc <= 10) {
+  res = res + inc * 7 + ' ';
+  inc++;
 }
+console.info(`i = ${res}`);
+
 console.log('Знайти суму всіх цілих чисел від 1 до 15');
-console.info(`${res4}`);
-
-res4 = 15;
-for (i = 16; i < 36; i++) {
-  res4 = res4 * i;
+res = 0;
+inc = 1;
+while (inc <= 15) {
+  res += inc;
+  inc++;
 }
+console.info(`i = ${res}`);
+
 console.log('Знайти добуток усіх цілих чисел від 15 до 35.');
-console.info(`${res4}`);
+res = 15;
+inc = 16;
+while (inc <= 35) {
+  res = res * inc;
+  inc++;
+}
+console.info(`i = ${res}`);
 
 console.log('Знайти середнє арифметичне всіх цілих чисел від 1 до 500.');
-res4 = 0;
-for (i = 1; i < 501; i++) {
-  res4 = res4 + i;
+res = 0;
+inc = 1;
+while (inc <= 500) {
+  res += inc;
+  inc++;
 }
-console.info(`${res4 / 500}`);
+console.info(`i = ${res / 500}`);
 
 console.log('Вивести суму лише парних чисел в діапазоні від 30 до 80.');
-res4 = 0;
-for (i = 30; i < 81; i++) {
-  if (i % 2 === 0) {
-    res4 = res4 + i;
+res = 0;
+inc = 30;
+while (inc <= 80) {
+  if (inc % 2 === 0) {
+    res += inc;
   }
+  inc++;
 }
-console.info(`${res4}`);
+console.log(`i = ${res}`);
 
 console.log('Вивести всі числа в діапазоні від 100 до 200 кратні 3');
-res4 = 0;
-for (i = 100; i < 201; i++) {
-  if (i % 3 === 0) {
-    res4 = res4 + i + ', ';
+res = '';
+inc = 100;
+while (inc <= 200) {
+  if (inc % 3 === 0) {
+    res += inc + ' ';
   }
+  inc++;
 }
-console.info(`${res4}`);
+console.log(`i = ${res}`);
 
 console.log('Дано натуральне число. Знайти та вивести на сторінку всі його дільники.');
-res4 = parseInt(prompt('Дано натуральне число',
+res = parseInt(prompt('Дано натуральне число',
   '26'));
-let result = '';
-for (i = 1; i <= res4; i++) {
-  if (res4 % i === 0) {
-    result = result + i + ', ';
+let result1 = '';
+inc = 1;
+while (inc !== res + 1) {
+  if (res % inc === 0) {
+    result1 += inc + ' ';
   }
+  inc++;
 }
-console.info(`${result}`);
+console.log(`${result1}`);
 
 console.log('Визначити кількість його парних дільників.');
 let count = 0;
 let sumOfOddDiv = 0;
 let test = 0;
-for (i = 1; i <= res4; i++) {
-  test = res4 % i;
+inc = 1;
+while (inc !== res + 1) {
+  test = res % inc;
   if (test === 0) {
-    if (i % 2 === 0) {
+    if (inc % 2 === 0) {
       count++;
-      sumOfOddDiv = sumOfOddDiv + i;
+      sumOfOddDiv = sumOfOddDiv + inc;
     }
   }
+  inc++;
 }
 console.info(`${count}`);
 
@@ -105,12 +124,18 @@ console.log('Знайти суму його парних дільників.');
 console.log(`${sumOfOddDiv}`);
 
 console.log('Надрукувати повну таблицю множення від 1 до 10.');
-result = '\n';
-for (i = 1; i < 10; i++) {
-  for (j = 1; j < 10; j++) {
-    result = result + (i * j) + ' ';
+let i = 1, j = 1;
+let result = '';
+while (i <= 10) {
+  while (j <= 10) {
+    result = result + (i * j) + ` `;
+    j++;
+
   }
   result += ',\n';
+  j = 1;
+  i++;
 }
 console.log(`${result}`);
+
 
